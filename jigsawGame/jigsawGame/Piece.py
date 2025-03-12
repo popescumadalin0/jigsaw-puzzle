6e258fe189bcd814434e83faaed71342f2bd26d8
janina280 <coceijanina@yahoo.com>
2025-03-12 22:52:12 +0200

    game board + ppt + game logic
    Signed-off-by: Madalin Popescu <dizzyforester@gmail.com>

--- jigsawGame/jigsawGame/Piece.py
+++
@@ -1,6 +0,0 @@
-class Piece:
-    def __init__(self, shape, width, height,color):
-        self.shape = shape
-        self.width = width
-        self.height = height
-        self.color = color
--- jigsawGame/jigsawGame/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from JigsawGame import JigsawGame
-from ControllerInput import ControllerInput
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = JigsawGame(root)
-    controller = ControllerInput(app)
-    root.mainloop()
